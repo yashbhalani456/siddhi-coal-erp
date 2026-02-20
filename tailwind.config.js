2cc74be64aa91ee3b4ba0956c2d05c0e70172f54
yashbhalani456 <81552397+yashbhalani456@users.noreply.github.com>
2026-02-21 01:43:24 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4f46e5"
+      }
+    }
+  },
+  plugins: []
+}
